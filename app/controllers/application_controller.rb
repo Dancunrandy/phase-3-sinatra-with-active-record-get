59ead083052cd2d517137df47f572654a0b61b8e
dancunrandy <dancunrandy@gmail.com>
2023-03-01 13:15:49 +0300

    commit tree bfa0c486259189dd010bcd947ad87fcc42cd305c

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < Sinatra::Base
+  get '/games' do
+    content_type :json
+    games = Game.all
 
-  get '/' do
-    { message: "Hello world" }.to_json
+    # return an array of JSON objects
+    games.to_json(only: [:id, :title, :platform, :genre, :price])
   end
 
+  get '/games/:id' do
+    content_type :json
+    game = Game.find(params[:id])
+
+    # include associated reviews and users in the JSON response
+    game.to_json(only: [:id, :title, :platform, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } },
+      users: { only: [:name] }
+    })
+  end
 end
